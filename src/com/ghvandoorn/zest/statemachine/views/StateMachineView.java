5745a5ad4eb6fca4ea88e7b80365e599e94d3ecc
Gerrit van Doorn <g.h.vandoorn@gmail.com>
2012-03-04 15:11:15 -0800

    Added IPartListener to draw graph when editor gets activated
    The view doesn't draw the graph yet but the logic to draw it when a
    XTextEditor is activatead (openend or switched to) was added

--- src/com/ghvandoorn/zest/statemachine/views/StateMachineView.java
+++ src/com/ghvandoorn/zest/statemachine/views/StateMachineView.java
@@ -1,15 +1,23 @@
 package com.ghvandoorn.zest.statemachine.views;
 
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IPartListener;
+import org.eclipse.ui.IPartService;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.zest.core.viewers.GraphViewer;
 
-public class StateMachineView extends ViewPart {
+public class StateMachineView extends ViewPart implements IPartListener {
 
 	private GraphViewer mViewer = null;
+	private XtextEditor mCurrentXtextEditor = null;
 
 	/**
 	 * The ID of the view as specified by the extension.
@@ -37,5 +45,43 @@ public class StateMachineView extends ViewPart {
 	 * Passing the focus request to the viewer's control.
 	 */
 	public void setFocus() {
+	}
+
+	@Override
+	public void partActivated(IWorkbenchPart part) {
+		IEditorPart editor = part.getSite().getPage().getActiveEditor();
+		if(editor != null && editor instanceof XtextEditor){
+			if (editor != mCurrentXtextEditor) { // In case we activate another editor/view
+				mCurrentXtextEditor = (XtextEditor)editor;
+				IXtextDocument document = mCurrentXtextEditor.getDocument();
+				drawGraph(document);
+			}
+		}
+	}
+
+	@Override
+	public void init(IViewSite site) throws PartInitException {
+		super.init(site);
+		IPartService service = (IPartService) getSite().getService(IPartService.class);
+		service.addPartListener(this);
+	}
+
+	private void drawGraph(IXtextDocument document) {
+	}
+
+	@Override
+	public void partBroughtToTop(IWorkbenchPart part) {
+	}
+
+	@Override
+	public void partClosed(IWorkbenchPart part) {
+	}
+
+	@Override
+	public void partDeactivated(IWorkbenchPart part) {
+	}
+
+	@Override
+	public void partOpened(IWorkbenchPart part) {
 	}
 }
