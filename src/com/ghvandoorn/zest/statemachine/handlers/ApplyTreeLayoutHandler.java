17df1e26e5efcf63d0b90d15e6d66941be205fdc
Gerrit van Doorn <g.h.vandoorn@gmail.com>
2012-03-05 16:16:40 -0800

    Added menu option to apply TreeLayoutAlgorithm
    Added a menu option and command that calls the ApplyTreeLayoutHandler to
    apply the Tree Layout Algorithm

---
+++ src/com/ghvandoorn/zest/statemachine/handlers/ApplyTreeLayoutHandler.java
@@ -0,0 +1,46 @@
+package com.ghvandoorn.zest.statemachine.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.ghvandoorn.zest.statemachine.views.StateMachineView;
+
+public class ApplyTreeLayoutHandler implements IHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		IWorkbenchPage page = window.getActivePage();
+		StateMachineView view = (StateMachineView) page.findView(StateMachineView.ID);
+		view.applyTreeLayout();
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+	}
+
+}
